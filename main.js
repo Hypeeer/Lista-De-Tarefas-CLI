4784e0f497502c8a73940611e1d2d3da86663032
Hypeeer <vittorjoao18@hotmail.com>
2024-11-02 14:00:51 -0300

    feat-adicionar-novas-tarefas-pronta

--- main.js
+++ main.js
@@ -2,24 +2,34 @@ import readlineSync from 'readline-sync';
 import addTasks from './src/functions/addTasks.js';
 
 const mainMenu = () => {
-  console.log(`Lista de tarefas`);
-  console.log(`[1] Adicionar Tarefa`);
-  console.log(`[2] Vizualizar Lista de Tarefas`);
-  console.log(`[3] Marca Tarefa Comcluidas`);
-  console.log(`[4] Remover Terefa`);
-  let choice = readlineSync.question(`Digite: `);
+  do {
+    console.log('Lista de tarefas');
+    console.log('[1] Adicionar Tarefa');
+    console.log('[2] Vizualizar Lista de Tarefas');
+    console.log('[3] Marca Tarefa Comcluidas');
+    console.log('[4] Remover Terefa');
+    console.log('[0] Encerra Programa');
 
-  switch (choice) {
-    case '1':
-      addTasks();
+    let choice = readlineSync.question('Digite: ');
+
+    switch (choice) {
+      case '1':
+        addTasks();
+        break;
+      case '2':
+        break;
+      case '3':
+        break;
+      case '4':
+        break;
+    }
+    if (choice === '0') {
+      console.log('Fim do programa!');
       break;
-    case '2':
-      break;
-    case '3':
-      break;
-    case '4':
-      break;
-  }
+    }
+  } while (true);
 };
 
 mainMenu();
+
+export default mainMenu;
--- src/functions/addTasks.js
+++ src/functions/addTasks.js
@@ -7,18 +7,20 @@ const addTasks = () => {
   object.id = readlineSync.question('\nId: ');
   object.name = readlineSync.question('name: ');
   object.description = readlineSync.question('Descrição: ');
-  console.log('\nTarefa salva com sucesso');
+  console.log('\nTarefa salva com sucesso!');
   saveTasks.push(object);
   exitMenu();
 };
 
 const exitMenu = () => {
   console.log('\nDeseja salvar outra tarefa ? ');
-  const userChoice = readlineSync.question('Digite: Sim (s) / Não (n) ');
+  const userChoice = readlineSync.question('Sim (s) / Não (n): ');
   if (userChoice === 's') {
     return addTasks();
   } else if (userChoice === 'n') {
-    console.log('Ate logo. Obrigado (a)! ');
+    console.log('\nDeseja volta para o menu pricipal ?');
+    const userChoice = readlineSync.question('Sim (s) / Não (n): ');
+    return userChoice === 's' ? console.log('\nVoltando para o menu...\n') : console.log('Obrigado (a)! Ate logo');
   }
 };
 
