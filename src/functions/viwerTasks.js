4f20e7955b0ff5423922934dec9f69251f211ef6
Hypeeer <vittorjoao18@hotmail.com>
2024-11-11 19:20:31 -0300

    funcao-para-chekar-entrada-vazia

--- src/functions/viwerTasks.js
+++ src/functions/viwerTasks.js
@@ -1,9 +1,19 @@
 import { saveTasks } from './addTasks.js';
 
 const displayTasksFormatted = () => {
-  saveTasks.forEach((saveTasks) => {
-    console.log(`Id: ${saveTasks.id} - Name: ${saveTasks.name} - Descrição: ${saveTasks.description} `);
+  const tasksToFormat = saveTasks;
+  checkIfTheFunctionIsEmpty(tasksToFormat);
+  tasksToFormat.forEach((saveTasks) => {
+    console.log(`\nID: ${saveTasks.id}`);
+    console.log(`Nome: ${saveTasks.name}`);
+    console.log(`Descrição: ${saveTasks.description}\n`);
   });
+};
+
+const checkIfTheFunctionIsEmpty = (saveTasks) => {
+  if (!saveTasks.length) {
+    console.log('\nAdicione tarefas antes de vizualizar na opção [1]! \n');
+  }
 };
 
 export default displayTasksFormatted;
