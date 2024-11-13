7aa2bf3f599231d2a91552ad6819b5dc0db7dc39
João Vitor <82682618+Hypeeer@users.noreply.github.com>
2024-11-12 23:05:41 -0300

    Delete src/functions/checkIfTheFunctionsIsEmpty.js
    ta errado

--- src/functions/checkIfTheFunctionsIsEmpty.js
+++
@@ -1,7 +0,0 @@
-const checkIfTheFunctionIsEmpty = (saveTasks) => {
-  if (!saveTasks.length) {
-    console.log('\nAdicione tarefas antes de vizualizar na opção [1]! \n');
-  }
-};
-
-export default checkIfTheFunctionIsEmpty;
